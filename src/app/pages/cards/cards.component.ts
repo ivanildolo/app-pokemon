fc903341a1b792709553b21e704649bfdbb564a2
ivanildolo <ivanildosilvalima@gmail.com>
2020-10-03 15:15:34 -0300

    Adjust filter cards

--- src/app/pages/cards/cards.component.ts
+++ src/app/pages/cards/cards.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CardsModel, CardModel } from 'src/app/models/card-model';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -12,24 +12,26 @@ import { HttpService } from 'src/app/services/http.service';
 export class CardsComponent implements OnInit {
   public cards: CardModel[] = [];
   public searchChanged: Subject<string> = new Subject<string>();
+  public filter = {
+    name: '',
+    page: 1,
+    pageSize: 100
+  }
   public loading: boolean = false;
   constructor(private httpService: HttpService) {}
 
   public onChangeSearch(event: any) {
-    console.log(event.target.value);
     this.searchChanged.next(event.target.value);
   }
 
   public getCards(searchName: string) {
-    let filter = {
-      name: searchName,
-      page: 1,
-      pageSize: 100
-    }
+    this.filter.name = searchName;
     this.loading = true;
-    this.httpService.getCards(filter)
+    this.httpService.getCards(this.filter)
       .subscribe((response: CardsModel) => {
         this.cards = response.cards;
+        console.log(this.cards);
+        
         this.loading = false;
       });
   }
