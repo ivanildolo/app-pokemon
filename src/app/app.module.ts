be4f745ba63a957ff4b9f8c3b7aca0d1fbb08d06
ivanildolo <ivanildosilvalima@gmail.com>
2020-10-02 22:50:32 -0300

    Ajust pages and diplay detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CardsComponent } from './pages/cards/cards.component';
+import { CardComponent } from './pages/card/card.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardsComponent,
+    CardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
